a702c46a24a81a206670094b877f76b87eb34804
Austin Davis <austindanieldavis@gmail.com>
2018-10-23 08:21:02 -0500

    added class names to headers

--- DOM-Drills/scripts.js
+++ DOM-Drills/scripts.js
@@ -6,13 +6,50 @@ document.addEventListener("DOMContentLoaded", function() {
     // h1 created
     let header1 = document.createElement('h1');
     let header1Text = document.createTextNode("This is an h1");
-
     header1.appendChild(header1Text);
     headerContainer.appendChild(header1);
-    document.body.appendChild(headerContainer);
 
     // h2 created
     let header2 = document.createElement('h2');
     let header2Text = document.createTextNode("This is an h2");
+    header2.appendChild(header2Text);
+    headerContainer.appendChild(header2);
+
+    // h3 created
+    let header3 = document.createElement('h3');
+    let header3Text = document.createTextNode("This is an h3");
+    header3.appendChild(header3Text);
+    headerContainer.appendChild(header3);
+
+    // h4 created
+    let header4 = document.createElement('h4');
+    let header4Text = document.createTextNode("This is an h4");
+    header4.appendChild(header4Text);
+    headerContainer.appendChild(header4);
+
+    // h5 created
+    let header5 = document.createElement('h5');
+    let header5Text = document.createTextNode("This is an h5");
+    header5.appendChild(header5Text);
+    headerContainer.appendChild(header5);
+
+    // h6 created
+    let header6 = document.createElement('h6');
+    let header6Text = document.createTextNode("This is an h6");
+    header6.appendChild(header6Text);
+    headerContainer.appendChild(header6);
+
+    document.body.appendChild(headerContainer);
+
+    // class names
+    document.getElementsByTagName('H1').className = 'h1';
+    console.log(document.getElementsByTagName('H1').className);
+    document.getElementsByTagName('H2').className = 'h2';
+    document.getElementsByTagName('H3').className = 'h3';
+    document.getElementsByTagName('H4').className = 'h3';
+    document.getElementsByTagName('H5').className = 'h4';
+    document.getElementsByTagName('H6').className = 'h5';
+
+
 
 });
