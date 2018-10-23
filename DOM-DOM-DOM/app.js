20807a6b1ccf7547281136f1bd68affe12924f2f
Austin Davis <austindanieldavis@gmail.com>
2018-10-23 16:48:24 -0500

    Started DOM DOM DOMMMM

--- DOM-DOM-DOM/app.js
+++ DOM-DOM-DOM/app.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let btnAddSquare = document.createElement('button');
+    let btnAddSquareText = document.createTextNode("Add Square");
+
+    btnAddSquare.style.margin = '2em';
+    
+    btnAddSquare.appendChild(btnAddSquareText);
+    document.body.appendChild(btnAddSquare);
+
+    btnAddSquare.addEventListener('click', function() {
+        let squareContainer = document.createElement('div');
+
+        let squareRow1 = document.createElement('div');
+
+        let squareBlack = document.createElement('div');
+        squareBlack.style.height = '100px';
+        squareBlack.style.width = '100px';
+        squareBlack.style.backgroundColor = 'black';
+        
+        squareRow1.appendChild(squareBlack);
+        squareContainer.appendChild(squareRow1);
+        document.body.appendChild(squareContainer);
+    });
 
 });
