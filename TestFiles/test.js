5ee515f8970347ea74687f42c217b7bda5c43764
Austin Davis <austindanieldavis@gmail.com>
2018-10-22 21:46:05 -0500

    Testing out DOM. Creating elements.

---
+++ TestFiles/test.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // CREATE the element (and text node in this case)
+    let paragraph = document.createElement('p');
+    let text = document.createTextNode('This can be whatever text you would like it to be.');
+    //
+
+    // MODIFY the element
+    paragraph.style.color = 'red';
+    paragraph.style.textTransform = 'uppercase';
+    paragraph.className = "some paragraph";
+    //
+
+    // INSERT the element
+    paragraph.appendChild(text);
+    document.body.appendChild(paragraph);
+    //
+
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('Click Me!');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+
+    button.addEventListener("click", function() {
+        let h1 = document.createElement('h1');
+        let h1Text = document.createTextNode("I've been clicked!");
+        h1.appendChild(h1Text);
+        document.body.appendChild(h1);
+
+    })
+
+});
+
